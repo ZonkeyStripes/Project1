a40dda649b694c13d559ae76f782f2f1e7bdf4c0
Maria Francis-Moullier <moullier@gmail.com>
2019-12-11 20:36:47 -0700

    pulling recipe name, thumbnail, ingredients from API

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -12,5 +12,39 @@ $("button").on("click", function() {
 
         console.log(response);
 
+        $("#recipes").empty();
+
+        for(let i = 0; i < 3; i++) {
+          let recipeName = $("<p>");
+          let recipeURL = $("<p>");
+          let recipeThumbnail = $("<img>");
+          recipeThumbnail.attr("src", response.hits[i].recipe.image);
+          $(recipeThumbnail).width(200);
+          $(recipeName).text(response.hits[i].recipe.label);
+          $(recipeName).css('font-weight', 'bold');
+          $(recipeURL).text(response.hits[i].recipe.url);
+
+          
+          $("#recipes").append(recipeName);
+          $("#recipes").append(recipeURL);
+          $("#recipes").append(recipeThumbnail);
+
+          let ingred = $("<ul>");
+          let ingredientList = response.hits[i].recipe.ingredients;
+          console.log(ingredientList);
+
+          // create ingredient list
+          for(let j = 0; j < ingredientList.length; j++) {
+            let newEl = $("<li>");
+            newEl.text(ingredientList[j].text);
+            ingred.append(newEl);
+
+          }
+          $("#recipes").append(ingred);
+
+        }
+
+        
+
       });
   });
